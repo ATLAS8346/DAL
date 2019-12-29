Refactor data pipeline + Fix accuracy computation

DAL_digits_release/datasets/dataset_read.py
@@ -7,51 +7,66 @@ from .mnist import load_mnist
 from .mnist_m import load_mnistm
 from .usps_ import load_usps
 from .synth_number import load_syn
+# from .synth_traffic import load_syntraffic
+# from .gtsrb import load_gtsrb
 
 
-def return_dataset(data, scale=False, usps=False, all_use='no'):
+def return_dataset(data, base_dir, scale=False, usps=False, all_use='no'):
+
     if data == 'svhn':
-        train_image, train_label, test_image, test_label = load_svhn()
-    if data == 'mnist':
-        train_image, train_label, test_image, test_label = load_mnist(scale=scale, usps=usps, all_use=all_use)
-        print(train_image.shape)
-    if data == 'usps':
-        train_image, train_label, test_image, test_label = load_usps(all_use=all_use)
-    if data == 'mnistm':
-        train_image, train_label, test_image, test_label = load_mnistm()
-    if data == 'synth':
-        train_image, train_label, test_image, test_label = load_syntraffic()
-    if data == 'gtsrb':
-        train_image, train_label, test_image, test_label = load_gtsrb()
-    if data == 'syn':
-        train_image, train_label, test_image, test_label = load_syn()
+        return load_svhn(base_dir)
 
-    return train_image, train_label, test_image, test_label
+    elif data == 'mnist':
+        return load_mnist(base_dir, scale=scale, usps=usps, all_use=all_use)
+
+    elif data == 'usps':
+         return load_usps(base_dir, all_use=all_use)
+
+    elif data == 'mnistm':
+        return load_mnistm(base_dir)
+
+    elif data == 'syn':
+        return load_syn(base_dir)
+
+    # elif data == 'synth':
+    #     return load_syntraffic(base_dir)
+
+    # elif data == 'gtsrb':
+    #     return load_gtsrb(base_dir)
+
+    else:
+        raise NotImplementedError("Dataset not found")
 
 
-def dataset_read(source, target, batch_size, scale=False, all_use='no'):
+def dataset_read(base_dir, source, target, batch_size, scale=False, all_use='no'):
 
     train_src, test_src = {}, {}
     train_trg, test_trg = {}, {}
 
     usps = True if source == 'usps' or target == 'usps' else False
+
     # domain_all = ['mnistm', 'mnist', 'usps', 'svhn', 'syn']
     domain_all = ['mnistm', 'mnist', 'usps', 'svhn']
     domain_all.remove(source)
+
+    # get source domain
     (train_src_img, train_src_label,
      test_src_img, test_src_label) = return_dataset(
-         source, scale=scale, usps=usps, all_use=all_use)
+         source, base_dir, scale=scale, usps=usps, all_use=all_use)
 
+    # get target domains
     train_trg_img, train_trg_label = [], []
     test_trg_img, test_trg_label = [], []
     for i in range(len(domain_all)):
         (_train_img, _train_label, _test_img, _test_label) = return_dataset(
-            domain_all[i], scale=scale, usps=usps, all_use=all_use)
+            domain_all[i], base_dir, scale=scale, usps=usps, all_use=all_use)
+
         train_trg_img.append(_train_img)
         train_trg_label.append(_train_label)
         test_trg_img.append(_test_img)
         test_trg_label.append(_test_label)
 
+    # aggregate all domains
     train_trg_img = np.concatenate(train_trg_img, axis=0)
     train_trg_label = np.concatenate(train_trg_label, axis=0)
     test_trg_img = np.concatenate(test_trg_img, axis=0)
@@ -80,12 +95,12 @@ def dataset_read(source, target, batch_size, scale=False, all_use='no'):
     test_trg['labels'] = test_trg_label
     scale = 32 if source == 'synth' else 32 if source == 'usps' or target == 'usps' else 32
 
-    train_loader = UnalignedDataLoader()
-    train_loader.initialize(train_src, train_trg, batch_size, batch_size, scale=scale)
+    train_loader = UnalignedDataLoader(
+        train_src, train_trg, batch_size, batch_size, scale=scale)
     dataset_train = train_loader.load_data()
 
-    test_loader = UnalignedDataLoader()
-    test_loader.initialize(test_src, test_trg, batch_size, batch_size, scale=scale)
+    test_loader = UnalignedDataLoader(
+        test_src, test_trg, batch_size, batch_size, scale=scale)
     dataset_test = test_loader.load_data()
 
     return dataset_train, dataset_test

DAL_digits_release/datasets/gtsrb.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cPickle as pkl
 
-def load_gtsrb():
+def load_gtsrb(base_dir):
     data_target = pkl.load(open('../data/data_gtsrb'))
     target_train = np.random.permutation(len(data_target['image']))
     data_t_im = data_target['image'][target_train[:31367], :, :, :]

DAL_digits_release/datasets/mnist.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.io import loadmat
 
-base_dir = './data/Digit-Five'
-def load_mnist(scale=True, usps=False, all_use=False):
+
+def load_mnist(base_dir, scale=True, usps=False, all_use=False):
     mnist_data = loadmat(base_dir + '/mnist_data.mat')
     if scale:
-        # print('I am loadinf from 32*32')
+        print('Loading MNIST 32x32')
         mnist_train = np.reshape(mnist_data['train_32'], (55000, 32, 32, 1))
         mnist_test = np.reshape(mnist_data['test_32'], (10000, 32, 32, 1))
         mnist_train = np.concatenate([mnist_train, mnist_train, mnist_train], 3)
@@ -15,9 +15,9 @@ def load_mnist(scale=True, usps=False, all_use=False):
         mnist_labels_train = mnist_data['label_train']
         mnist_labels_test = mnist_data['label_test']
     else:
+        print('Loading MNIST 28x28')
         mnist_train = mnist_data['train_28']
-        mnist_test =  mnist_data['test_28']
-        # print('i am loading from 28*28')
+        mnist_test = mnist_data['test_28']
         mnist_train = np.concatenate([mnist_train, mnist_train, mnist_train], 3)
         mnist_test = np.concatenate([mnist_test, mnist_test, mnist_test], 3)
         mnist_labels_train = mnist_data['label_train']
@@ -26,15 +26,17 @@ def load_mnist(scale=True, usps=False, all_use=False):
         mnist_test = mnist_test.astype(np.float32)
         mnist_train = mnist_train.transpose((0, 3, 1, 2))
         mnist_test = mnist_test.transpose((0, 3, 1, 2))
+
     train_label = np.argmax(mnist_labels_train, axis=1)
     inds = np.random.permutation(mnist_train.shape[0])
     mnist_train = mnist_train[inds]
     train_label = train_label[inds]
     test_label = np.argmax(mnist_labels_test, axis=1)
 
-    print('mnist train X shape->',  mnist_train.shape)
-    print('mnist train y shape->',  train_label.shape)
-    print('mnist test X shape->',  mnist_test.shape)
+    print('mnist train X shape->', mnist_train.shape)
+    print('mnist train y shape->', train_label.shape)
+    print('mnist test X shape->', mnist_test.shape)
     print('mnist test y shape->', test_label.shape)
+    print("====================")
 
     return mnist_train, train_label, mnist_test, test_label

DAL_digits_release/datasets/mnist_m.py
@@ -5,7 +5,7 @@ base_dir = './data/Digit-Five'
 def load_mnistm(scale=True, usps=False, all_use=False):
     mnistm_data = loadmat(base_dir + '/mnistm_with_label.mat')
     mnistm_train = mnistm_data['train']
-    mnistm_test =  mnistm_data['test']
+    mnistm_test = mnistm_data['test']
     mnistm_train = mnistm_train.transpose(0, 3, 1, 2).astype(np.float32)
     mnistm_test = mnistm_test.transpose(0, 3, 1, 2).astype(np.float32)
     mnistm_labels_train = mnistm_data['label_train']
@@ -21,8 +21,9 @@ def load_mnistm(scale=True, usps=False, all_use=False):
     # train_label = train_label[:25000]
     # mnistm_test = mnistm_test[:9000]
     # test_label = test_label[:9000]
-    print('mnist_m train X shape->',  mnistm_train.shape)
-    print('mnist_m train y shape->',  train_label.shape)
-    print('mnist_m test X shape->',  mnistm_test.shape)
+    print('mnist_m train X shape->', mnistm_train.shape)
+    print('mnist_m train y shape->', train_label.shape)
+    print('mnist_m test X shape->', mnistm_test.shape)
     print('mnist_m test y shape->', test_label.shape)
+    print("====================")
     return mnistm_train, train_label, mnistm_test, test_label

DAL_digits_release/datasets/svhn.py
@@ -4,9 +4,9 @@ import sys
 
 sys.path.append('./utils/')
 from utils.utils import dense_to_one_hot
-# from scipy.misc import imresize
-base_dir = './data/Digit-Five'
-def load_svhn():
+
+
+def load_svhn(base_dir):
     svhn_train = loadmat(base_dir + '/svhn_train_28x28.mat')
     svhn_test = loadmat(base_dir + '/svhn_test_28x28.mat')
     svhn_train_im = svhn_train['X']
@@ -15,9 +15,11 @@ def load_svhn():
     svhn_test_im = svhn_test['X']
     svhn_test_im = svhn_test_im.transpose(3, 2, 0, 1).astype(np.float32)
     svhn_label_test = dense_to_one_hot(svhn_test['y'])
-    print('svhn train X shape->',  svhn_train_im.shape)
-    print('svhn train y shape->',  svhn_label.shape)
-    print('svhn test X shape->',  svhn_test_im.shape)
+
+    print('svhn train X shape->', svhn_train_im.shape)
+    print('svhn train y shape->', svhn_label.shape)
+    print('svhn test X shape->', svhn_test_im.shape)
     print('svhn test y shape->', svhn_label_test.shape)
+    print("====================")
 
     return svhn_train_im, svhn_label, svhn_test_im, svhn_label_test

DAL_digits_release/datasets/synth_traffic.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cPickle as pkl
 
-def load_syntraffic():
+def load_syntraffic(base_dir):
     data_source = pkl.load(open('../data/data_synthetic'))
     source_train = np.random.permutation(len(data_source['image']))
     data_s_im = data_source['image'][source_train[:len(data_source['image'])], :, :, :]

DAL_digits_release/datasets/unaligned_data_loader.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 from datasets_ import Dataset
 
 
-class PairedData(object):
+class PairedData():
     def __init__(self, data_loader_A, data_loader_B, max_dataset_size):
         self.data_loader_A = data_loader_A
         self.data_loader_B = data_loader_B
@@ -50,29 +50,26 @@ class PairedData(object):
 
 
 class UnalignedDataLoader():
-    def initialize(self, source, target, batch_size1, batch_size2, scale=32):
+    def __init__(self, source, target, bs_source, bs_target, scale=32):
+
         transform = transforms.Compose([
             transforms.Resize(scale),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
-        dataset_source = Dataset(source['imgs'], source['labels'], transform=transform)
-        dataset_target = Dataset(target['imgs'], target['labels'], transform=transform)
-        data_loader_s = torch.utils.data.DataLoader(
-            dataset_source,
-            batch_size=batch_size1,
-            shuffle=True,
-            num_workers=4)
 
-        data_loader_t = torch.utils.data.DataLoader(
-            dataset_target,
-            batch_size=batch_size2,
-            shuffle=True,
-            num_workers=4)
-        self.dataset_s = dataset_source
-        self.dataset_t = dataset_target
-        self.paired_data = PairedData(data_loader_s, data_loader_t,
-                                      float("inf"))
+        self.dataset_s = Dataset(source['imgs'], source['labels'], transform=transform)
+        self.dataset_t = Dataset(target['imgs'], target['labels'], transform=transform)
+
+        loader_s = torch.utils.data.DataLoader(
+            self.dataset_s, batch_size=bs_source,
+            shuffle=True, num_workers=4)
+
+        loader_t = torch.utils.data.DataLoader(
+            self.dataset_t, batch_size=bs_target,
+            shuffle=True, num_workers=4)
+
+        self.paired_data = PairedData(loader_s, loader_t, float("inf"))
 
     def name(self):
         return 'UnalignedDataLoader'

DAL_digits_release/datasets/usps_.py
@@ -5,10 +5,10 @@ import pickle
 import sys
 sys.path.append('../utils/')
 from utils.utils import dense_to_one_hot
-base_dir = './data/Digit-Five'
 
-def load_usps(all_use=False):
-    dataset  = loadmat(base_dir + '/usps_28x28.mat')
+
+def load_usps(base_dir, all_use=False):
+    dataset = loadmat(base_dir + '/usps_28x28.mat')
     data_set = dataset['dataset']
     img_train = data_set[0][0]
     label_train = data_set[0][1]
@@ -26,12 +26,12 @@ def load_usps(all_use=False):
     label_train = dense_to_one_hot(label_train)
     label_test = dense_to_one_hot(label_test)
     img_train = np.concatenate([ img_train, img_train, img_train], 1)
-    img_test = np.concatenate([img_test, img_test,img_test],1)
+    img_test = np.concatenate([img_test, img_test, img_test], 1)
 
     print('usps train X shape->',  img_train.shape)
     print('usps train y shape->',  label_train.shape)
     print('usps test X shape->',  img_test.shape)
     print('usps test y shape->', label_test.shape)
-
+    print("====================")
 
     return img_train, label_train, img_test, label_test

DAL_digits_release/main.py
@@ -1,62 +1,47 @@
-from __future__ import print_function
 import argparse
 import torch
 import sys
+import os
+
+from solver_disentangle import Solver
+from pprint import pprint
+
 sys.path.append('./model')
 sys.path.append('./datasets')
 sys.path.append('./metric')
 
-from solver_disentangle import Solver
-import os
 
 # Training settings
-parser = argparse.ArgumentParser(description='PyTorch MCD Implementation')
-parser.add_argument('--all_use', type=str, default='no', metavar='N',
-                    help='use all training data? in usps adaptation')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--checkpoint_dir', type=str, default='checkpoint', metavar='N',
-                    help='source only or not')
-parser.add_argument('--eval_only', action='store_true', default=False,
-                    help='evaluation only option')
-parser.add_argument('--lr', type=float, default=0.0002, metavar='LR',
-                    help='learning rate (default: 0.0002)')
-parser.add_argument('--max_epoch', type=int, default=200, metavar='N',
-                    help='how many epochs')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--num_k', type=int, default=4, metavar='N',
-                    help='hyper paremeter for generator update')
-parser.add_argument('--one_step', action='store_true', default=False,
-                    help='one step training with gradient reversal layer')
+parser = argparse.ArgumentParser(description='PyTorch DAL Implementation')
+parser.add_argument('--all_use', type=str, default='no', metavar='N', help='use all training data? in usps adaptation')
+parser.add_argument('--checkpoint_dir', type=str, default='checkpoint')
+parser.add_argument('--data_dir', type=str, default='./data/Digit-Five')
+parser.add_argument('--eval_only', action='store_true', default=False, help='evaluation only option')
+
+parser.add_argument('--max_epoch', type=int, default=200, metavar='N', help='how many epochs')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N', help='input batch size for training (default: 64)')
+parser.add_argument('--lr', type=float, default=0.0002, metavar='LR', help='learning rate (default: 0.0002)')
+parser.add_argument('--num_k', type=int, default=4, metavar='N', help='hyper paremeter for generator update')
+
 parser.add_argument('--optimizer', type=str, default='adam', metavar='N', help='which optimizer')
-parser.add_argument('--resume_epoch', type=int, default=100, metavar='N',
-                    help='epoch to resume')
-parser.add_argument('--save_epoch', type=int, default=10, metavar='N',
-                    help='when to restore the model')
-parser.add_argument('--save_model', action='store_true', default=False,
-                    help='save_model or not')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--source', type=str, default='svhn', metavar='N',
-                    help='source dataset')
+parser.add_argument('--resume_epoch', type=int, default=100, metavar='N', help='epoch to resume')
+parser.add_argument('--save_epoch', type=int, default=10, metavar='N', help='when to restore the model')
+parser.add_argument('--save_model', action='store_true', default=False, help='save_model or not')
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+
+parser.add_argument('--source', type=str, default='svhn', metavar='N', help='source dataset')
 parser.add_argument('--target', type=str, default='agnostic', metavar='N', help='target dataset')
-parser.add_argument('--use_abs_diff', action='store_true', default=False,
-                    help='use absolute difference value as a measurement')
-parser.add_argument('--recordfolder', type=str, default='source_only', metavar='N', help='target dataset')
+parser.add_argument('--use_abs_diff', action='store_true', default=False, help='use absolute difference value as a measurement')
+
 parser.add_argument('--exp_name', type=str, default='test', metavar='N')
-parser.add_argument('--gpu', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--use_cuda', action='store_true', default=True,
-                    help='Use cuda or not')
+parser.add_argument('--use_cuda', action='store_true', default=True, help='Use cuda or not')
 
 args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
+args.cuda = args.use_cuda and torch.cuda.is_available()
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
-    torch.cuda.set_device(args.gpu)
-print(args)
+pprint(args)
 
 def main():
     solver = Solver(args, source=args.source, target=args.target,
@@ -76,8 +61,8 @@ def main():
             if epoch % 1 == 0:
                 print("Epoch {}".format(epoch))
                 print("==========================")
-                solver.test(epoch, subset='train', save_model=args.save_model)
-                solver.test(epoch, subset='test', save_model=args.save_model)
+                solver.test(subset='train', save_model=args.save_model)
+                solver.test(subset='test', save_model=args.save_model)
                 print("==========================")
             if global_step >= 20000:
                 break

DAL_digits_release/solver_disentangle.py
@@ -59,7 +59,7 @@ class Solver(nn.Module):
 
         print('Loading datasets')
         self.dataset_train, self.dataset_test = dataset_read(
-            source, target, self.batch_size,
+            args.data_dir, source, target, self.batch_size,
             scale=self.scale, all_use=self.all_use)
         print('Done!')
 
@@ -141,12 +141,16 @@ class Solver(nn.Module):
 
     def optimize_classifier(self, img_src, label_src):
         _loss = dict()
+        _acc = dict()
         feat_src = self.G(img_src)
         for key in ['ds', 'di', 'ci']:
             k = "class_src_%s" % key
-            _loss[k] = self.xent_loss(
-                self.C[key](self.D[key](feat_src)), label_src)
+            out = self.C[key](self.D[key](feat_src))
+            _loss[k] = self.xent_loss(out, label_src)
+            _acc[k] = (out.detach().max(1)[1] == label_src).float().sum() / out.size(0)
+
         self.log_scalar(_loss, prefix='class_loss')
+        self.log_scalar(_acc, prefix='train_acc')
 
         _sum_loss = sum([l for _, l in _loss.items()])
         _sum_loss.backward()
@@ -304,8 +308,7 @@ class Solver(nn.Module):
         self.group_opt_step(['D_di', 'D_ci', 'D_ds', 'R'])
         return rec_loss_src, rec_loss_trg
 
-    def train_epoch(self, epoch):
-        # set training
+    def set_train(self):
         for k in self.components.keys():
             self.components[k].train()
         for k in self.C.keys():
@@ -313,6 +316,17 @@ class Solver(nn.Module):
         for k in self.D.keys():
             self.D[k].train()
 
+    def set_eval(self):
+        for k in self.components.keys():
+            self.components[k].eval()
+        for k in self.C.keys():
+            self.C[k].eval()
+        for k in self.D.keys():
+            self.D[k].eval()
+
+    def train_epoch(self, epoch):
+        # set training
+        self.set_train()
         # torch.cuda.manual_seed(1)
         total_batches = 500000
         pbar_descr_prefix = "Epoch %d" % (epoch)
@@ -345,13 +359,8 @@ class Solver(nn.Module):
                 self.global_step += 1
         return self.global_step
 
-    def test(self, epoch, subset='test', save_model=False):
-        self.G.eval()
-        self.D['di'].eval()
-        self.D['ds'].eval()
-        self.C['di'].eval()
-        self.C['ds'].eval()
-
+    def test(self, subset='test', save_model=False):
+        self.set_eval()
         dataset = self.dataset_test if subset == 'test' else self.dataset_train
         with torch.no_grad():
             loss_src, loss_trg = dict(), dict()
@@ -366,7 +375,7 @@ class Solver(nn.Module):
                 img_src, label_src = img_src.to(self.device), label_src.to(self.device)
 
                 img_trg, label_trg = data['T'], data['T_label'].long()
-                img_trg, label_trg = img_src.to(self.device), label_src.to(self.device)
+                img_trg, label_trg = img_trg.to(self.device), label_trg.to(self.device)
 
                 out_src, out_trg = dict(), dict()
                 pred_src, pred_trg = dict(), dict()
@@ -383,8 +392,8 @@ class Solver(nn.Module):
                     loss_src[key] += F.cross_entropy(out_src[key], label_src, reduction='sum').item()
                     loss_trg[key] += F.cross_entropy(out_trg[key], label_trg, reduction='sum').item()
                     # correct predictions
-                    correct_src[key] += pred_src[key].eq(label_src.data).cpu().sum()
-                    correct_trg[key] += pred_trg[key].eq(label_trg.data).cpu().sum()
+                    correct_src[key] += pred_src[key].eq(label_src).cpu().sum()
+                    correct_trg[key] += pred_trg[key].eq(label_trg).cpu().sum()
 
                 size_src += label_src.data.size()[0]
                 size_trg += label_trg.data.size()[0]
@@ -392,7 +401,7 @@ class Solver(nn.Module):
         print("Source - {}".format(subset))
         acc, loss = dict(), dict()
         for key in ['di', 'ds', 'ci']:
-            acc[key] = correct_src[key] / size_src
+            acc[key] = float(correct_src[key]) / size_src
             loss[key] = loss_src[key] / size_src
             print("\t{key}: acc = {acc:.2f}, loss = {loss:.4f}".format(key=key, acc=acc[key], loss=loss[key]))
         self.log_scalar(acc, prefix='{}_src_acc'.format(subset))
@@ -401,7 +410,7 @@ class Solver(nn.Module):
         print("Target - {}".format(subset))
         acc, loss = dict(), dict()
         for key in ['di', 'ds', 'ci']:
-            acc[key] = correct_trg[key] / size_src
+            acc[key] = float(correct_trg[key]) / size_src
             loss[key] = loss_trg[key] / size_src
             print("\t{key}: acc = {acc:.2f}, loss = {loss:.4f}".format(key=key, acc=acc[key], loss=loss[key]))
         self.log_scalar(acc, prefix='{}_trg_acc'.format(subset))